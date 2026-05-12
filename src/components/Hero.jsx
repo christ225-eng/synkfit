f67612d6b0a3ce04c1131733c2506bdea3762b12
christ-eng <dongochrist225@gmail.com>
2026-05-12 20:54:31 +0200

    modif

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,8 +44,8 @@ function Hero() {
       <div className="hero-visual">
         <div className="hero-image-card">
           <img
-            src="IMAGE\WhatsApp Image 2026-04-15 at 01.19.08.jpeg"
-            alt="Athlète en pleine session de musculation"
+            src="https://plus.unsplash.com/premium_photo-1672046218369-67e12ed1c364?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Les chaussures de sport SynkFit en action, avec des données de fréquence cardiaque affichées en temps réel."
           />
 
           <div className="hero-stats">
